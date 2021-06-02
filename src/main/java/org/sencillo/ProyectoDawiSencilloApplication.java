af61587571d626ce1a0e67858b3610ed027364d9
beemo1251 <beemo1251@hotmail.com>
2021-06-01 22:25:07 -0500

    creacion del proyecto spring

---
+++ src/main/java/org/sencillo/ProyectoDawiSencilloApplication.java
@@ -0,0 +1,13 @@
+package org.sencillo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDawiSencilloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDawiSencilloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sencillo/ProyectoDawiSencilloApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sencillo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDawiSencilloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
