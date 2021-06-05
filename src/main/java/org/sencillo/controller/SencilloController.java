a954fbc2e2c57c2a93ec8ea527679b238394b05e
angeloleon <elangelusvlr@gmail.com>
2021-06-05 13:43:25 -0500

    ProductoDetails Pt1

--- src/main/java/org/sencillo/controller/SencilloController.java
+++ src/main/java/org/sencillo/controller/SencilloController.java
@@ -8,43 +8,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-public class ProyectoController {
+public class SencilloController {
 	
 	@Autowired
 	private IProductoRepository repo;
 	
-	/*@PostMapping("/grabar")
-	public String guardarPeli(@ModelAttribute Pelicula pelicula, RedirectAttributes redirectAttrs) {
-		System.out.println(pelicula);
-		try {
-			repo.save(pelicula);
-			redirectAttrs
-        	.addFlashAttribute("mensaje", "Actualizado correctamente")
-        	.addFlashAttribute("clase", "success");
-		} catch (Exception e) {
-			redirectAttrs
-        	.addFlashAttribute("error", "Ocurrio un error")
-        	.addFlashAttribute("clase", "danger");
-		}
-		return "redirect:/listado";
-	}*/
-	
 	@GetMapping("/index")
 	public String listarProd(@ModelAttribute Producto producto, Model model) {
 		model.addAttribute("lstProducto", repo.findAll());
-		System.out.println(repo.findAll());
+		//System.out.println(repo.findAll());
 		return "index";
 	}
-	
-	/*@GetMapping("/index")
-	public String listarProd(Model model) {
-		System.out.println("index");
-		return "index";
-	}*/
 	
 	@GetMapping("/login")
 	public String login(Model model) {
@@ -59,8 +35,12 @@ public class ProyectoController {
 	}
 	
 	@GetMapping("/detail")
-	public String detail(Model model) {
-		System.out.println("detail");
+	public String detail(@ModelAttribute Producto p, Model model) {
+		model.addAttribute("codProducto", repo.findById(p.getCodProducto()));
+		model.addAttribute("lstProducto", repo.findAll());
 		return "detail";
 	}
+
+	
+	
 }
