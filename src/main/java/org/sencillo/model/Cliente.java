1fe1337f27cbddf1f33f01fdb3112ac9d84c58ff
gabo <gr8785424@gmail.com>
2021-06-06 21:46:12 -0500

    Avance de registro

--- src/main/java/org/sencillo/model/Cliente.java
+++ src/main/java/org/sencillo/model/Cliente.java
@@ -26,4 +26,60 @@ public class Cliente {
 	
 	@Column(name="id_usuario")
 	private int codUsuario;
+
+	@Override
+	public String toString() {
+		return "Cliente [codCliente=" + codCliente + ", nombre=" + nombre + ", apellido=" + apellido + ", direccion="
+				+ direccion + ", fchNac=" + fchNac + ", codUsuario=" + codUsuario + "]";
+	}
+
+	public String getCodCliente() {
+		return codCliente;
+	}
+
+	public void setCodCliente(String codCliente) {
+		this.codCliente = codCliente;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getFchNac() {
+		return fchNac;
+	}
+
+	public void setFchNac(String fchNac) {
+		this.fchNac = fchNac;
+	}
+
+	public int getCodUsuario() {
+		return codUsuario;
+	}
+
+	public void setCodUsuario(int codUsuario) {
+		this.codUsuario = codUsuario;
+	}
+	
+	
 }
